fa98924991483dbdf2cb3f6c15057cbd61db1389
Mike <lanselot777hailander@gmail.com>
2019-07-16 20:51:02 -0400

    Errors

--- Errores en python/errors.py
+++ Errores en python/errors.py
@@ -1,6 +1,9 @@
-class MyCustomError(TypeError):
+class MyCustomError(Exception):
+    """
+    Exception raised when a specific error code is needed.
+    """
     def __init__(self, message, code):
-        super().__init__(f'Eroor code {code}: {message}')
+        super().__init__(f'Error code {code}: {message}')
         self.code = code
 
 raise MyCustomError('OUCH! An error happened.', 500)
