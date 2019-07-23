ca43a517fbc1bf53444163325d0a798069290434
Mike <lanselot777hailander@gmail.com>
2019-07-23 09:14:08 -0400

    Typing in python

--- milestone_2_lists/utils/database.py
+++ milestone_2_lists/utils/database.py
@@ -1,5 +1,6 @@
 #import json
 #import sqlite3
+from typing import List, Dict, Union
 from .database_connection import DatabaseConnection
 """
 Concerned with storing and retrieving books from a list.
@@ -7,6 +8,8 @@ Format of the csv file:
 
 name,author,read\n
 """
+
+Book = Dict(str, Union(str, int)) #a new Type created
 ############ Save into CSV file ############
 #books_file = 'books.txt'
 
@@ -91,7 +94,7 @@ name,author,read\n
 ############ Save into JSON file ############
 
 ############ Save into SQLITE3 ############
-def create_book_table():
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
@@ -99,7 +102,7 @@ def create_book_table():
 
 
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
@@ -108,7 +111,7 @@ def add_book(name, author):
 
 
 
-def get_all_books():
+def get_all_books() -> List[Book]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
@@ -116,13 +119,13 @@ def get_all_books():
         books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] #[{name, author, read},(name, author, read)]
     return books
 
-def mark_book_as_read(name):
+def mark_book_as_read(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
-def delete_book(name):
+def delete_book(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
