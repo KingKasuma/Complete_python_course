1f55a3f5c8e31796d225adc5207f52619a9e1141
Mike <lanselot777hailander@gmail.com>
2019-07-23 07:39:49 -0400

    Milistone project with SQLITE3

--- milestone_2_lists/utils/database.py
+++ milestone_2_lists/utils/database.py
@@ -1,4 +1,6 @@
-import json
+#import json
+#import sqlite3
+from .database_connection import DatabaseConnection
 """
 Concerned with storing and retrieving books from a list.
 Format of the csv file:
@@ -53,37 +55,77 @@ name,author,read\n
 ############ Save into CSV file ############
 
 ############ Save into JSON file ############
-books_file = 'books.json'
+#books_file = 'books.json'
 
 
+#def create_book_table():
+#    with open(books_file, 'w') as file:
+#        json.dump([], file)
+
+
+#def add_book(name, author):
+#    books = get_all_books()
+#    books.append({'name': name, 'author': author, 'read': False})
+#    _save_all_books(books)
+
+
+#def get_all_books():
+#    with open(books_file, 'r') as file:
+#        return json.load(file)
+
+#def  _save_all_books(books):
+#    with open(books_file, 'w') as file:
+#        json.dump(books, file)
+
+#def mark_book_as_read(name):
+#    books = get_all_books()
+#    for book in books:
+#        if book['name'] == name:
+#            book['read'] = True
+#    _save_all_books(books)
+
+#def delete_book(name):
+#    books = get_all_books()
+#    books = [book for book in books if book['name'] != name]
+#    _save_all_books(books)
+############ Save into JSON file ############
+
+############ Save into SQLITE3 ############
 def create_book_table():
-    with open(books_file, 'w') as file:
-        json.dump([], file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
+
+
 
 
 def add_book(name, author):
-    books = get_all_books()
-    books.append({'name': name, 'author': author, 'read': False})
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute(f'INSERT INTO books VALUES(?,?, 0)', (name, author))
+
+
 
 
 def get_all_books():
-    with open(books_file, 'r') as file:
-        return json.load(file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-def  _save_all_books(books):
-    with open(books_file, 'w') as file:
-        json.dump(books, file)       
+        cursor.execute('SELECT * FROM books')
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] #[{name, author, read},(name, author, read)]
+    return books
 
 def mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
-############ Save into JSON file ############
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('DELETE from books WHERE name=?', (name,))
+
+############ Save into SQLITE3 ############
